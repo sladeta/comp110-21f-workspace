90a6913bf04de34d76dc012da3d0020b556e8151
Thomas Slade <sladeta18@gmail.com>
2021-09-15 18:24:02 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730236019"
+
+word: str = input("Enter a word: ")
+i: int = 0
+h: int = 0
+answer: bool = False
+ch: str
+
+while i < len(word):
+    ch = word[i]
+    while ((h < len(word)) and (i != h)): 
+        if(ch == word[i]):  
+            response = True
+        h += 1
+    h = 0
+    i += 1
+final_answer: str = str(answer)
+
+print("Found Duplicate: " + final_answer)
