b1be08e643bbbe6c61d1c45707e27cce1de33afc
Thomas Slade <sladeta18@gmail.com>
2021-10-04 16:17:33 -0400

    commit

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,45 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730236019"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    """Returns only even numbers within a list."""
+    even_list: list[int] = list()
+    i: int = 0
+    list_len: int = len(x)
+    while i < list_len: 
+        if x[i] % 2 == 0:
+            even_list.append(x[i])
+        i += 1
+    return even_list
+
+
+def sub(input: list[int], start: int, end: int) -> list[int]:
+    subset: list[int] = list()
+    list_len: int = len(input)
+    if list_len == 0 or start > list_len or end <= 0:
+        return subset
+    if end > list_len:
+        end == list_len - 1
+    if start < 0:
+        start == 0
+    while start < end:
+        subset.append(input[start])
+        start += 1
+    return subset
+
+
+def concat(x: list[int], y: list[int]) -> list[int]:
+    i: int = 0
+    len_x: int = len(x)
+    len_y: int = len(y)
+    answer: list[int] = list()
+    while i < len_x: 
+        answer.append(x[i])
+        i += 1
+    h: int = 0
+    while h < len_y:
+        answer.append(y[h])
+        h += 1
+    return answer
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,25 @@
 """Unit tests for list utility functions."""
 
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+
+def test_sub_01() -> None:
+    test_list: list[int] = [10, 20]
+    assert sub(test_list, 1, 2) == [20]
+
+
+def test_sub_02() -> None:
+    test_list: list[int] = [10, 20, 30]
+    assert sub(test_list, 0, 1) == [10]
+
+
+def test_sub_03() -> None:
+    test_list: list[int] = [10, 20, 30, 40]
+    assert sub(test_list, 0, 4) == [10, 20, 30, 40]
